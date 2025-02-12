83e36870713df8484773d7e893db743e79f58dc2
InfiniumKnight <nasaman1954@gmail.com>
2025-02-12 12:53:50 -0500

    Early version of movement system

---
+++ Assets/CameraControler.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControler : MonoBehaviour
+{
+   
+    public float sensitivity;
+
+    public Camera cam;
+
+    float rotY = 0f;
+
+    float rotX = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotY += Input.GetAxis("Mouse X") * sensitivity;
+        rotX += Input.GetAxis("Mouse Y") * sensitivity;
+
+        rotX = Mathf.Clamp(rotX, -70, 90);
+
+        transform.localEulerAngles = new Vector3(0, rotY, 0);
+        cam.transform.localEulerAngles = new Vector3(-rotX, 0, 0);
+    }
+}
